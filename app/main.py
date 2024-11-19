5805dcd33e0296cbc87044839dab99b37eb8e62a
alkalink1 <143008344+alkalink1@users.noreply.github.com>
2024-11-19 03:13:15 +0000

    dp1

--- app/main.py
+++ app/main.py
@@ -1,41 +1,46 @@
 #!/usr/bin/env python3
 
-from fastapi import FastAPI
-# from typing import Optional
-# from pydantic import BaseModel
-import mysql.connector
-from mysql.connector import Error
+from fastapi import Request, FastAPI
+from typing import Optional
+from pydantic import BaseModel
 import json
 import os
-from mysql.connector import RefreshOption
+import mysql
+import mysql.connector
+from mysql.connector import Error
 
-refresh = RefreshOption.LOG | RefreshOption.THREADS
+
+app = FastAPI()
 
 DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
 DBUSER = "admin"
 DBPASS = os.getenv('DBPASS')
-DB = "nem2p"
+DB = "eju2pk"
 
-db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
 cur=db.cursor()
 
-app = FastAPI()
+@app.get("/")  # zone apex
+def zone_apex():
+    return {"Good Day": "Sunshine!"}
 
-from fastapi.middleware.cors import CORSMiddleware
+@app.get("/sum/{a}/{b}")
+def add(a: int, b: int):
+    return {"sum": a + b}
+
+@app.get("/multiply/{c}/{d}")
+def multiply(c: int, d: int):
+    return {"product": c * d}
+
+from fastapi.middleware. cors import CORSMiddleware 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Good Day": "Sunshine!"}
-
 @app.get('/genres')
-async def get_genres():
-    db.cmd_refresh(refresh)
+def get_genres():
     query = "SELECT * FROM genres ORDER BY genreid;"
     try:    
         cur.execute(query)
@@ -46,14 +51,12 @@ async def get_genres():
             json_data.append(dict(zip(headers,result)))
         return(json_data)
     except Error as e:
-        print("MySQL Error: ", str(e))
         return {"Error": "MySQL Error: " + str(e)}
-
+    
 @app.get('/songs')
-async def get_genres():
-    db.cmd_refresh(refresh)
-    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid;"
-    try:    
+def get_songs():
+    query = "Select * From songs ORDER BY id;"
+    try:
         cur.execute(query)
         headers=[x[0] for x in cur.description]
         results = cur.fetchall()
@@ -63,4 +66,4 @@ async def get_genres():
         return(json_data)
     except Error as e:
         print("MySQL Error: ", str(e))
-        return None
+        return {"Error": "MySQL Error: " + str(e)}  
