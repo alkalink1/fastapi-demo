f8a96bd46279f16b4809ac4cc863b97457051107
alkalink1 <143008344+alkalink1@users.noreply.github.com>
2024-11-19 03:33:13 +0000

    dp1again

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from fastapi import Request, FastAPI
+from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 import json
@@ -8,9 +8,7 @@ import os
 import mysql
 import mysql.connector
 from mysql.connector import Error
-
-
-app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
 DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
 DBUSER = "admin"
@@ -20,11 +18,20 @@ DB = "eju2pk"
 db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
 cur=db.cursor()
 
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Good Day": "Sunshine!"}
+app = FastAPI()
 
-@app.get("/sum/{a}/{b}")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= ['*'],
+    allow_methods= ['*'],
+    allow_headers= ['*'],
+)
+@app.get("/")
+def zone_apex():
+    return {"Greetings": "Breadloaf"}
+
+@app.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
 
@@ -32,27 +39,40 @@ def add(a: int, b: int):
 def multiply(c: int, d: int):
     return {"product": c * d}
 
-from fastapi.middleware. cors import CORSMiddleware 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.get("/data")
+async def get_data():
+    example_data = {
+        "id" : 1,
+        "name" : "Pesto",
+        "animal" : "Penguin"
+    }
+    return example_data
+
+@app.get("/story")
+async def get_story():
+    storytime = {
+        "story": "There once was a lad, that's it that the story :)"
+    }
+    return storytime
+
+
+#get methods for Spotify
+ 
 @app.get('/genres')
 def get_genres():
     query = "SELECT * FROM genres ORDER BY genreid;"
-    try:    
+    try:
         cur.execute(query)
         headers=[x[0] for x in cur.description]
         results = cur.fetchall()
         json_data=[]
         for result in results:
             json_data.append(dict(zip(headers,result)))
-        return(json_data)
+            return(json_data)
     except Error as e:
+        print("MySQL Error: ", str(e))
         return {"Error": "MySQL Error: " + str(e)}
-    
+
 @app.get('/songs')
 def get_songs():
     query = "Select * From songs ORDER BY id;"
@@ -66,4 +86,4 @@ def get_songs():
         return(json_data)
     except Error as e:
         print("MySQL Error: ", str(e))
-        return {"Error": "MySQL Error: " + str(e)}  
+        return {"Error": "MySQL Error: " + str(e)}
